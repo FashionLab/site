8ae04cbd10c7c229705174b487bd3528eca29676
Tim Paine <t.paine154@gmail.com>
2019-12-22 10:34:58 -0500

    more lint fixes

--- src/tests/components/Main.test.js
+++ src/tests/components/Main.test.js
@@ -1,17 +1,19 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from '../../components/Main';
 import { BrowserRouter } from 'react-router-dom';
-import { createMuiTheme } from '@material-ui/core/styles'
-import {ThemeProvider} from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import Main from '../../components/Main';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(
-        <ThemeProvider theme={createMuiTheme()}>
-            <BrowserRouter >
-                <Main />
-            </BrowserRouter>
-        </ThemeProvider>, div);
-    ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <ThemeProvider theme={createMuiTheme()}>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </ThemeProvider>, div,
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
