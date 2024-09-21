83bc76696b94ec12a3cffbe2c81d543ffbbe5cc6
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-09-21 17:13:21 -0400

    Move to sveltekit

---
+++ src/lib/home/Contact.svelte
@@ -0,0 +1,31 @@
+<section id="contact" class="mb-2 text-neutral-200 text-center">
+	<div
+		class="mx-auto bg-no-repeat bg-cover"
+		style="background-position: 75%; background-image: url('img/contact-bg.jpg');"
+	>
+		<div
+			class="relative isolate overflow-hidden px-6 pt-16 sm:px-16 md:pt-24 lg:gap-x-20 lg:px-24 lg:pt-0 backdrop-blur-lg"
+		>
+			<div class="mx-auto text-center lg:mx-0 lg:flex-auto lg:py-32">
+				<h2 class="text-4xl font-bold tracking-tight text-white sm:text-8xl">Find out more.</h2>
+				<p class="mt-6 text-2xl leading-8 text-white">
+					FashionLab is currently under active development
+					<br />
+					Reach out via email, or learn more on our GitHub.
+				</p>
+				<div class="mt-10 flex items-center justify-center gap-x-6">
+					<a
+						href="mailto:dev@fashionlab.ai?subject=More Information"
+						class="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-neutral-900 shadow-sm hover:bg-neutral-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+						>Reach Out</a
+					>
+					<a
+						href="https://github.com/fashionlab"
+						class="text-base font-semibold leading-7 text-white"
+						>Learn more <span aria-hidden="true">→</span></a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
